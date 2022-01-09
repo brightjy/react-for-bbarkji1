d18d318977ac87a58a93b5398b0df95fb83b1504
박지영 <barkji0@naver.com>
2022-01-09 15:41:53 +0900

    make to do list using useState([]), list.map()

--- src/App.js
+++ src/App.js
@@ -5,12 +5,13 @@ function App() {
   const [toDos, setToDos] = useState([]);
   const onChange = (event) => setToDo(event.target.value);
   const onSubmit = (event) => {
-    event.preventDefault(); // 해당 이벤트를 처리하는 동안에는 이벤트 수신+동작 X
+    event.preventDefault(); //해당 이벤트를 처리하는 동안에는 이벤트 수신+동작 X
     if (toDo === "") {
       return;
     }
-    setToDos(currentArray => [toDo, ...currentArray]); // currentArray를 받아와서 toDo 추가
-    setToDo("");  // state를 직접적으로 수정하지는 않는다. 함수를 통해 초기화
+    // 함수를 보낼 때 react.js는 함수의 첫 번째 argument로 현재의 state를 보낸다.
+    setToDos((currentArray) => [toDo, ...currentArray]); //currentArray를 받아와서 toDo 추가
+    setToDo("");  //state를 직접적으로 수정하지는 않는다. 함수를 통해 초기화
   };
   console.log(toDos);
   return (
@@ -24,6 +25,10 @@ function App() {
           placeholder="write your to do..." />
         <button>Add To Do</button>
       </form>
+      <hr />
+      {toDos.map((item, index) => 
+        <li kye={index}>{item}</li>
+      )};
     </div>
   );
 }
