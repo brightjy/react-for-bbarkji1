c2634c7b226fa4fcf1d5bc6e0df00e134f2b94f3
박지영 <barkji0@naver.com>
2022-01-10 20:35:54 +0900

    coin tracker - useEffet, useState, coinpaprika API

--- src/App.js
+++ src/App.js
@@ -1,34 +1,30 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 function App() {
-  const [toDo, setToDo] = useState("");
-  const [toDos, setToDos] = useState([]);
-  const onChange = (event) => setToDo(event.target.value);
-  const onSubmit = (event) => {
-    event.preventDefault(); //해당 이벤트를 처리하는 동안에는 이벤트 수신+동작 X
-    if (toDo === "") {
-      return;
-    }
-    // 함수를 보낼 때 react.js는 함수의 첫 번째 argument로 현재의 state를 보낸다.
-    setToDos((currentArray) => [toDo, ...currentArray]); //currentArray를 받아와서 toDo 추가
-    setToDo("");  //state를 직접적으로 수정하지는 않는다. 함수를 통해 초기화
-  };
-  console.log(toDos);
+  const [loading, setLoading] = useState(true);
+  const [coins, setCoins] = useState([]);
+  useEffect(() => {
+    fetch("https://api.coinpaprika.com/v1/tickers")
+    .then((reponse) => reponse.json())
+    .then((json) => {
+      setCoins(json);
+      setLoading(false);
+    });
+  }, []); //처음에 한 번만 보여줌(아무것도 지켜보지 않음.)
   return (
     <div>
-      <h1>My To Dos ({toDos.length})</h1>
-      <form onSubmit={onSubmit}>
-        <input  
-          value={toDo} 
-          onChange={onChange} 
-          type="text" 
-          placeholder="write your to do..." />
-        <button>Add To Do</button>
-      </form>
-      <hr />
-      {toDos.map((item, index) => 
-        <li kye={index}>{item}</li>
-      )};
+      <h1>The Coins! {loading ? "" : `(${coins.length})`}</h1>
+      {loading ? 
+        <strong>Loading...</strong> 
+        : 
+        <select>
+          {coins.map((coin) => (
+            <option>
+              {coin.name} ({coin.symbol}): {coin.quotes.USD.price} USD
+            </option>
+          ))}
+        </select>
+      }
     </div>
   );
 }
